296ad5f66fdaeb8557d185e3221be595a8946f30
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2024-03-18 21:47:07 +0530

    Js with HTML basics

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let firstHeading = document.querySelector('h1');
+
+
+// change the text
+
+// innerText, innerHTML
+
+firstHeading.innerText = "Abhishek Shankar Choudhary"
+
+// firstHeading.innerHTML = "Abhishek <span> Shankar </span> <br> Choudhary"
+
+
+// change the style(css)
+// firstHeading.style.color = "red";
+// firstHeading.style.backgroundColor = "black";
+// firstHeading.style.textAlign= "center";
+
+
+// firstHeading.style.cssText = `
+// background-color: black;
+// color: red;
+// text-align: center;
+// `
